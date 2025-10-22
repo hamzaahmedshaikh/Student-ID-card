cf076adcc4b473ff266c70fc6afd4dd09704ffad
hamzaahmedshaikh <mrhamzaahmedshaikh@gmail.com>
2025-10-23 04:24:56 +0500

    commit

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-function generateAdmitCard() {
-      
-      const student = {
-        name: prompt("Enter your full name:"),
-        age: prompt("Enter your age:"),
-        id: prompt("Enter your student ID:"),
-        course: prompt("Enter your course:"),
-        className: prompt("Enter your class name:"),
-        photo: prompt("Enter your profile photo URL (optional):") || "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg/220px-Arnold_Schwarzenegger_by_Gage_Skidmore_4.jpg"
-      };
+function GenerateCard() {
+  const user = {
+    name: prompt("Enter your name"),
+    age: prompt("Enter your age"),
+    ID: prompt("Enter your ID"),
+    Course: prompt("Enter your Course"),
+    Batch: prompt("Enter your Batch")
+  };
 
-      document.getElementById("admitCard").style.display = "block";
+  document.getElementById("student_id").style.display = "block";
+  document.getElementById("startBtn").style.display = "none";
+  document.querySelector("label[for='upload']").style.display = "none";
 
-      document.getElementById("name").innerHTML = `<span class="bold">Name:</span> ${student.name}`;
-      document.getElementById("age").innerHTML = `<span class="bold">Age:</span> ${student.age}`;
-      document.getElementById("studentId").innerHTML = `<span class="bold">ID:</span> ${student.id}`;
-      document.getElementById("course").innerHTML = `<span class="bold">Course:</span> ${student.course}`;
-      document.getElementById("class").innerHTML = `<span class="bold">Class:</span> ${student.className}`;
-      document.getElementById("photo").src = student.photo;
-    }
+  document.getElementById("name").innerHTML = user.name;
+  document.getElementById("age").innerHTML = `<span class="bold">Age:</span> <span class="value">${user.age}</span>`;
+  document.getElementById("ID").innerHTML = `<span class="bold">ID:</span> <span class="value">${user.ID}</span>`;
+  document.getElementById("Course").innerHTML = `<span class="bold">Course:</span> <span class="value">${user.Course}</span>`;
+  document.getElementById("Batch").innerHTML = `<span class="bold">Batch:</span> <span class="value">${user.Batch}</span>`;
+}
+
+function uploadImage(event) {
+  var file = event.target.files[0];
+  var img = document.getElementById("profilePhoto");
+  img.src = URL.createObjectURL(file);
+}
